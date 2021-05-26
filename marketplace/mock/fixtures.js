64e9a4a18100634181179b83e8946054d2d9dfab
Flavien Busseuil <flavien@classcraft.com>
2021-05-26 22:47:38 +0200

    Fixtures

---
+++ marketplace/mock/fixtures.js
@@ -0,0 +1,64 @@
+export default {
+  nbWaitingTables: 2,
+  tables: [
+    {
+      gameName: "Dungeon Petz",
+      tableImg:
+        "https://x.boardgamearena.net/data/themereleases/210520-0804/games/dungeonpetz/210506-0041/img/game_icon.png",
+      link: "",
+      isWaitingCurrentPlayer: true,
+      players: [
+        {
+          playerName: "anotherFlav",
+          isActivePlayer: true,
+        },
+        {
+          playerName: "AriusC",
+          isActivePlayer: false,
+        },
+        {
+          playerName: "Talion4",
+          isActivePlayer: false,
+        },
+        {
+          playerName: "Domiin",
+          isActivePlayer: true,
+        },
+      ],
+    },
+    {
+      gameName: "Splendor",
+      tableImg:
+        "https://x.boardgamearena.net/data/themereleases/210520-0804/games/splendor/210516-0818/img/game_icon.png",
+      link: "",
+      isWaitingCurrentPlayer: true,
+      players: [
+        {
+          playerName: "anotherFlav",
+          isActivePlayer: true,
+        },
+        {
+          playerName: "AriusC",
+          isActivePlayer: false,
+        },
+      ],
+    },
+    {
+      gameName: "Super Fantasy Brawl",
+      tableImg:
+        "https://x.boardgamearena.net/data/themereleases/current/games/superfantasybrawl/210415-1135/img/game_icon.png",
+      link: "",
+      isWaitingCurrentPlayer: false,
+      players: [
+        {
+          playerName: "anotherFlav",
+          isActivePlayer: false,
+        },
+        {
+          playerName: "Dravalha",
+          isActivePlayer: true,
+        },
+      ],
+    },
+  ],
+};
