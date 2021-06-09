fb928a9fb72761587e5d607abe1266b67e3f222b
Flavien Busseuil <flavien@classcraft.com>
2021-06-09 16:28:06 +0200

    release commands and script

---
+++ scripts/release.zsh
@@ -0,0 +1,21 @@
+# Set a new version number
+yarn version;
+# Update the version number in manifest
+node scripts/updateManifestVersionFromPackage.js;
+# Set a new TAG name from version number
+TAG=v$(node scripts/getVersion.js);
+# production build
+yarn build;
+# zip
+zip builds/build-$TAG.zip build;
+# commit changes
+git add package.json manifest.jon;
+git commit -m 'Update version';
+git push;
+# create a new release
+gren release --tags $TAG;
+# create and push a changelog
+gren changelog --override;
+git add CHANGELOG.md;
+git commit -m 'Update Changelog';
+git push
