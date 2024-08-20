dd1e7f74bb325d39fc2af36c2efd1b3a00e84ab8
cdelaforge <root@ALMAVIA097-WIN.almavia.fr>
2024-08-20 11:11:59 +0200

    Firefox compatibility

--- scripts/updateManifestVersionFromPackage.js
+++ scripts/updateManifestVersionFromPackage.js
@@ -1,12 +1,24 @@
 const fs = require("fs");
 const manifest = require("../src/manifest.json");
+const manifestFF = require("../src/manifest-firefox.json");
 const package = require("../package.json");
 
 manifest.version = package.version;
+manifestFF.version = package.version;
 
 fs.writeFile(
 	"./src/manifest.json",
 	JSON.stringify(manifest, null, 2),
+	(err) => {
+		if (err) {
+			throw err;
+		}
+	},
+);
+
+fs.writeFile(
+	"./src/manifest-firefox.json",
+	JSON.stringify(manifestFF, null, 2),
 	(err) => {
 		if (err) {
 			throw err;
