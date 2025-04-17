4eb53128e7c2408f5ac9526ff41a0c6b8857890a
nalka <alan.audivert@hotmail.fr>
2025-04-17 15:16:33 +0200

    Living forest kodama dark theme fix

--- src/js/config/darkThemeGames.ts
+++ src/js/config/darkThemeGames.ts
@@ -4228,6 +4228,7 @@ _darkStyleForGame['livingforest'] = `
 body { background: none !important; }
 .icon_fragment { filter: invert(1); }
 .lvf_playerboard_name { background: var(--dark-back); color: #fff; }
+div[id^=lvf_respanel_counter_kodama_] { color: white }
 `;
 
 _darkStyleForGame['livingforestduel'] = `
