3787dc5a4c6005f0250751937036eab3876eebc1
GhoulofGSG9 <ghoulofgsg9@gmail.com>
2025-05-31 14:44:59 +0200

    refactor: Make other content scripts type safe
    Additional improve forum script by aligning it to the melodice script

--- src/contentForum.ts
+++ src/contentForum.ts
@@ -2,10 +2,20 @@ import Configuration from "./js/config/configuration";
 import { setDarkStyle } from "./js/ui/content/darkMode/darkStyleForumFunctions";
 import { changeDarkColors } from "./js/ui/content/darkMode/darkColors";
 
+let windowLoaded = false;
+let configLoaded = false;
+
+const sendForumLoaded = () => {
+  if (windowLoaded && configLoaded) {
+    // hack to avoid light theme flashing
+    top!.postMessage({ key: 'bga_ext_forum_visible' }, 'https://boardgamearena.com/');
+  }
+};
+
 window.addEventListener("load", () => {
-  // hack to avoid light theme flashing
-  top.postMessage({ key: 'bga_ext_forum_visible' }, 'https://boardgamearena.com/');
-});
+  windowLoaded = true;
+  sendForumLoaded();
+})
 
 const adjustDarkColors = () => {
   const hue = config.getDarkModeColor('forum');
@@ -22,11 +32,18 @@ const initPage = () => {
   if (config.isSolidBackground()) {
     document.documentElement.classList.add('bgaext_solid_back');
   }
+
+  configLoaded = true;
+  sendForumLoaded();
 };
 
 document.addEventListener('bga_ext_update_config', (data) => {
-  if (['darkModeColor', 'darkModeSat'].includes(data.detail.key)) {
+  const key = (data as CustomEvent).detail.key as string;
+
+  if (key === 'dark') {
     adjustDarkColors();
+  } else if (key === 'darkMode') {
+    setDarkStyle(config.isDarkMode(), config.getCustomCss());
   }
 });
 
