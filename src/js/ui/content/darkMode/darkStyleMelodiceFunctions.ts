96e97baf403d1de4017acc5195fdfaf73f22b162
Christophe DELAFORGE <christophe.delaforge@almaviacx.com>
2025-08-13 09:50:50 +0200

    Fix melodice style

--- src/js/ui/content/darkMode/darkStyleMelodiceFunctions.ts
+++ src/js/ui/content/darkMode/darkStyleMelodiceFunctions.ts
@@ -10,30 +10,27 @@ Promise.all(cssList.map(file => getFile(file))).then(fileContents => {
 	fileContents.forEach(({ file, content }) => cssContents[file] = content);
 });
 
-const _setDarkStyle = (returnFunc: () => void) => {
+const _setStyle = (returnFunc: () => void) => {
 	if (cssContents["dark_theme/melodice.css"]) {
-		console.log("[bga extension - melodice] Set dark mode");
 		styleComponent.innerHTML = `${cssContents["dark_theme/melodice.css"]}${customCssCode}`;
 		returnFunc();
 	} else {
-		setTimeout(() => _setDarkStyle(returnFunc), 100);
+		setTimeout(() => _setStyle(returnFunc), 100);
 	}
-};
-
-const _setLightStyle = () => {
-	console.log("[bga extension - melodice] Set light mode");
-	styleComponent.innerHTML = customCssCode;
 };
 
 const _setDarkOrLightStyle = (returnFunc: () => void, val: boolean, customCss: string) => {
+	const theme = document.documentElement.dataset.theme;
+	const newTheme = val ? 'dark' : 'light'
+
 	customCssCode = customCss;
 
-	if (val) {
-		_setDarkStyle(returnFunc);
-	} else {
-		_setLightStyle();
-		returnFunc();
+	if (theme !== newTheme) {
+		console.log(`[bga extension - melodice] Set ${newTheme} mode`);
+		document.documentElement.dataset.theme = newTheme;
 	}
+
+	_setStyle(returnFunc);
 };
 
 export const setDarkStyle = async (val: boolean, customCss: string): Promise<void> => {
